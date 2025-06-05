17e2ae7cd87caad90232c8f64b2b981569176124
Vanitha <vanithanaidu198@gmail.com>
2025-06-05 08:11:32 +0530

    special-var.sh

--- special-vars.sh
+++ special-vars.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 echo "All variables passed to the script: $@"
-
+echo "Number of variables: $#"
+echo "Script name: $0"
+echo "First argument : $1"
+echo $2
+echo "current working directory of script : $PWD"
+echo "home directory of user: $HOME"
+echo "user runing the script: $USER"
+echo "PID of current script: $$"
+sleep 10 &
+echo "PID of last command run in background: $!"
