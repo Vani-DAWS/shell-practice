bab281a3378d76363de418f300e01863dc6f65f7
Vanitha <vanithanaidu198@gmail.com>
2025-06-15 13:01:33 +0530

    install-command.sh

--- install-command.sh
+++ install-command.sh
@@ -2,4 +2,24 @@
 
 USERID=$(id -u)
 
-echo $USERID
+if [ $USERID -ne 0 ]
+then
+    echo "Please run with root user"
+    exit 1
+else
+    echo "User is a root user and proceed with installation"
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+    echo "mysql is not installed and proceed with installation"
+    dnf install mysql -y
+    if [ $? -eq 0]
+    echo "installation successful"
+    else 
+    echo "installation failed"
+    exit 1
+    fi
+else
+    echo "mysql already installed"
+fi
