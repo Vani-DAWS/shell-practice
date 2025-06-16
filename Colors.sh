ff0bae21c5103575bf5c816913b0dc751d3117d9
Vanitha <vanithanaidu198@gmail.com>
2025-06-16 19:45:44 +0530

    Colors.sh

---
+++ Colors.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$Y Please run with root user"
+    exit 1
+else
+    echo -e "$G User is a root user and proceed with installation"
+fi
+
+# dnf install nginx -y
+#     if [ $? -eq 0]
+#     then
+#     echo "installation successful"
+#     exit
+#     else 
+#     echo "installation failed"
+#     fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo "$y mysql is not installed and proceed with installation"
+    dnf install mysql -y
+    if [ $? -eq 0]
+    then
+    echo "$G installation successful"
+    exit 1
+    else 
+    echo "$R Error:: $N installation failed"
+    fi
+else
+    echo "$Y mysql already installed"
+fi
