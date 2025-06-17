cc0b97af856aba22c3e85c73dc8407b2cad9284a
Vanitha <vanithanaidu198@gmail.com>
2025-06-17 11:52:16 +0530

    07-logs.sh

---
+++ 07-logs.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="var/log/shellscript-logs"
+SCRIPT_NAME="echo $0 | cut -d "." f1"
+LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing" : $(date)" &>>$LOGS_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$Y Please run with root user" &>>$LOGS_FILE
+    exit 1
+else
+    echo -e "$G User is a root user and proceed with installation" &>>$LOGS_FILE
+fi
+
+validate(){
+    if [ $1 -eq 0]
+    then
+    echo "$G installation of $2 successful" &>>$LOGS_FILE
+    exit 1
+    else 
+    echo "$R Error:: $N $2 installation failed" &>>$LOGS_FILE
+    fi
+}
+
+
+dnf list installed nginx &>>$LOGS_FILE
+if [ $? -ne 0 ]
+then
+    echo "$y nginx is not installed and proceed with installation" &>>$LOGS_FILE
+    dnf install nginx -y &>>$LOGS_FILE
+    validate $? "nginx"
+else
+    echo "$Y nginx already installed" &>>LOGS_FILE &>>$LOGS_FILE
+fi
+
+dnf list installed mysql &>>$LOGS_FILE
+if [ $? -ne 0 ]
+then
+    echo "$y mysql is not installed and proceed with installation" &>>$LOGS_FILE
+    dnf install mysql -y &>>$LOGS_FILE
+    validate $? "mysql"
+else
+    echo "$Y mysql already installed" &>>$LOGS_FILE
+fi
+
+
