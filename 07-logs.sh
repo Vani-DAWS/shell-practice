e3b3f9ba4bcedce3b8949aa920d35816b03ca39e
Vanitha <vanithanaidu198@gmail.com>
2025-06-17 12:25:47 +0530

    07-logs.sh

--- 07-logs.sh
+++ 07-logs.sh
@@ -12,7 +12,7 @@ SCRIPT_NAME="echo $0 | cut -d "." f1"
 LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
-echo "Script started:$(date)" &>>$LOGS_FILE
+echo "Script started:$(date)" | tee -a $LOGS_FILE
 
 if [ $USERID -ne 0 ]
 then
